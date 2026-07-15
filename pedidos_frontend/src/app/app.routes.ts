21fc0162b6a59811afd30a888d9e3acffd60773e
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-07-14 22:33:38 -0300

    feat(auth): add authentication page with styles and base styles
    - Created auth.page.html with placeholder text for the authentication page.
    - Added auth.page.scss for styling the authentication page, including a background image.
    - Implemented auth.page.ts as a standalone Angular component for the authentication page.
    - Introduced base.scss for global styles, defining color variables and resetting margins and paddings.

--- pedidos_frontend/src/app/app.routes.ts
+++ pedidos_frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () => import('./pages/auth/auth.page').then((m) => m.AuthPage),
+  },
+];
