599f6005ef790eecd5611e1623d5e909e9192de2
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-07-15 14:20:58 -0300

    feat(auth): implement auth guard for protected routes; add dashboard and orders pages

--- pedidos_frontend/src/app/app.routes.ts
+++ pedidos_frontend/src/app/app.routes.ts
@@ -1,8 +1,28 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './http/services/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadComponent: () => import('./pages/auth/auth.page').then((m) => m.AuthPage),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
+  },
+  {
+    path: 'orders',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/orders/order.page').then((m) => m.OrderPage),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
   },
 ];
