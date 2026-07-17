29d46897c66b68c3863b123e1fccddeabf799da1
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-07-17 13:17:22 -0300

    feat(interceptors, dashboard): add auth interceptor and log loaded orders in dashboard

--- pedidos_frontend/src/app/app.config.ts
+++ pedidos_frontend/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
 
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authInterceptor } from './http/interceptors/authInterceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideCharts(withDefaultRegisterables()),
 
     {
